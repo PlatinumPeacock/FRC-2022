53e234bdfff99c7ab93e1b103b6742564c7f32ea
Austin Catholic <platinumpeacockspc8115@gmail.com>
2022-02-09 18:16:23 -0500

    Create DriveToDistance.java

---
+++ gradle/src/main/java/frc/robot/commands/DriveToDistance.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrainTrial;
+
+public class DriveToDistance extends CommandBase {
+  DriveTrainTrial driveTrain;
+  private boolean finish = false;
+  /** Creates a new DriveToDistance. */
+  public DriveToDistance(DriveTrainTrial dt) {
+    driveTrain = dt;
+    addRequirements(driveTrain);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    finish = driveTrain.driveToDistance(Constants.SETPOINT_FORWARD, Constants.AUTONOMOUS_SPEED);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveTrain.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return finish;
+  }
+}
