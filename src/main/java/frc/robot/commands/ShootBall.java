506c960c92e290f4fff7d5e003e334d1e18b047d
Austin Catholic <platinumpeacockspc8115@gmail.com>
2022-02-09 18:21:42 -0500

    Create ShootBall.java

---
+++ src/main/java/frc/robot/commands/ShootBall.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Shooter;
+
+public class ShootBall extends CommandBase {
+  Shooter shooter;
+  double shooterSpeed = Constants.SHOOTER_SPEED;
+
+  /** Creates a new ShootBall. */
+  public ShootBall(Shooter s) {
+    shooter = s;
+    addRequirements(shooter);
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shooter.shootBall(shooterSpeed);
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+   shooter.slow();
+   shooter.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
