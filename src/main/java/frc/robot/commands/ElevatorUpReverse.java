35fb3393d864121740b407ccf5562619331ed7fa
Austin Catholic <platinumpeacockspc8115@gmail.com>
2022-02-09 18:20:45 -0500

    Create ElevatorUpReverse.java

---
+++ src/main/java/frc/robot/commands/ElevatorUpReverse.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Elevator;
+
+public class ElevatorUpReverse extends CommandBase {
+  Elevator elevator;
+  /** Creates a new ElevatorUp. */
+  public ElevatorUpReverse(Elevator e) {
+    elevator = e;
+    
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    elevator.elevatorUpReverse(Constants.ELEVATOR_SPEED);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    elevator.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
